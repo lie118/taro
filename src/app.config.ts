d38058fce650c297f93a633a3af073009cf9ffcf
lie118 <1352356138@qq.ocm>
2023-05-22 18:52:22 +0800

    tabBar image

--- src/app.config.ts
+++ src/app.config.ts
@@ -7,11 +7,21 @@ export default defineAppConfig({
     navigationBarTextStyle: "black",
   },
   tabBar: {
-    selectedColor:'#0000c2',
+    selectedColor: "#00BFFF",
+    color: "#333",
     list: [
-      { text: "首页", pagePath: "pages/index/index" },
-      { text: "详情", pagePath: "pages/user/index" },
-      { text: "用户", pagePath: "pages/detail/index" },
+      {
+        text: "首页",
+        pagePath: "pages/index/index",
+        iconPath: "./assets/tabBar/home.png",
+        selectedIconPath: "./assets/tabBar/select-home.png",
+      },
+      {
+        text: "用户",
+        pagePath: "pages/user/index",
+        iconPath: "./assets/tabBar/user.png",
+        selectedIconPath: "./assets/tabBar/select-user.png",
+      },
     ],
   },
 });
